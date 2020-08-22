56962dc7ff740373c22acf3708015b58bf8f0241
Renato Groffe <renato.groffe@yahoo.com.br>
2020-08-22 09:09:03 -0300

    ASP.NET Core 3.1 + Site MVC
    ASP.NET Core 3.1 + Site MVC

---
+++ SiteBaixadaNerd/Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using SiteBaixadaNerd.Models;
+
+namespace SiteBaixadaNerd.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            _logger.LogInformation("Acesso à View Index");
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
